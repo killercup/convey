ab2953872818632628681702fd96b16d9d54e68c
Pascal Hertleif <killercup@gmail.com>
2018-10-04 17:41:24 +0200

    Add custom struct example
    Oh god this is so verbose right now… someone should write a macro!

---
+++ examples/custom.rs
@@ -0,0 +1,63 @@
+extern crate failure;
+extern crate output;
+#[macro_use]
+extern crate serde_derive;
+
+use output::{
+    components::{color, text},
+    human, json,
+};
+use std::fs::File;
+
+fn main() -> Result<(), failure::Error> {
+    let mut out = output::new()
+        .add(json::file("target/foo.log")?)
+        .add(human::stdout()?);
+
+    #[derive(Serialize)]
+    struct ErrorMessage {
+        code: i32,
+        name: String,
+        message: String,
+    }
+
+    use failure::Error;
+    use output::RenderOutput;
+
+    impl RenderOutput for ErrorMessage {
+        fn render_for_humans(&self, fmt: &mut human::Formatter) -> Result<(), Error> {
+            color()
+                .add_item(
+                    color()
+                        .fg("red")?
+                        .bg("black")?
+                        .add_item(text(self.code.to_string()))
+                        .add_item(text(" ")),
+                )
+                .add_item(
+                    color()
+                        .fg("red")?
+                        .bg("black")?
+                        .add_item(text(self.name.clone())),
+                )
+                .add_item(text("\n> "))
+                .add_item(text(self.message.clone()))
+                .render_for_humans(fmt)?;
+
+            Ok(())
+        }
+
+        fn render_json(&self, fmt: &mut json::Formatter) -> Result<(), Error> {
+            fmt.write(self)?;
+            Ok(())
+        }
+    }
+
+    out.print(&ErrorMessage {
+        code: 42,
+        name: String::from("error"),
+        message: String::from("Oh god no"),
+    })?;
+
+    Ok(())
+}
