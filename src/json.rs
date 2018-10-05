d28762e34d20976812542ab8dbd76fc3f1318919
Pascal Hertleif <killercup@gmail.com>
2018-10-05 21:29:12 +0200

    Add test helper for json
    Not the most convenient one, as I just notice. I should probably try and
    make something that collects into a `Vec<T>` where `T` is something I
    can easily compare with `assert_eq!`.

--- src/json.rs
+++ src/json.rs
@@ -17,6 +17,8 @@ pub fn file<T: AsRef<Path>>(name: T) -> Result<Target, Error> {
     }))
 }
 
+pub use self::test_helper::test;
+
 /// JSON formatter
 pub struct Formatter {
     pub(crate) writer: Box<Write>,
@@ -65,6 +67,75 @@ macro_rules! render_json {
         fn render_json(&self, fmt: &mut $crate::json::Formatter) -> Result<(), $crate::Error> {
             fmt.write(self)?;
             Ok(())
+        }
+    }
+}
+
+mod test_helper {
+    use super::Formatter;
+    use termcolor::Buffer;
+    use test_buffer::TestBuffer;
+    use Target;
+
+    /// Create a test output target
+    ///
+    /// **Note:** This is intended for usage in tests; this and the methods you can call out in will
+    /// often just panic instead of return `Result`s.
+    ///
+    /// # Usage
+    ///
+    /// ```rust
+    /// #[macro_use] extern crate output;
+    /// #[macro_use] extern crate serde_derive;
+    ///
+    /// fn main() -> Result<(), output::Error> {
+    ///     let test_target = output::json::test();
+    ///     let mut out = output::new().add_target(test_target.target());
+    ///
+    ///     #[derive(Serialize)]
+    ///     struct Message {
+    ///         author: String,
+    ///         body: String,
+    ///     }
+    ///     impl output::Render for Message {
+    ///         render_for_humans!(this -> []);
+    ///         render_json!();
+    ///     }
+    ///
+    ///     out.print(Message { author: "Pascal".into(), body: "Lorem ipsum dolor".into() })?;
+    ///
+    ///     assert_eq!(
+    ///         test_target.to_string(),
+    ///         "{\"author\":\"Pascal\",\"body\":\"Lorem ipsum dolor\"}\n",
+    ///     );
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn test() -> TestTarget {
+        TestTarget {
+            buffer: Buffer::no_color().into(),
+        }
+    }
+
+    pub struct TestTarget {
+        buffer: TestBuffer,
+    }
+
+    impl TestTarget {
+        pub fn formatter(&self) -> Formatter {
+            Formatter {
+                writer: Box::new(self.buffer.clone()),
+            }
+        }
+
+        pub fn target(&self) -> Target {
+            Target::Json(self.formatter())
+        }
+
+        pub fn to_string(&self) -> String {
+            let target = self.buffer.0.clone();
+            let buffer = target.read().unwrap();
+            String::from_utf8_lossy(buffer.as_slice()).to_string()
         }
     }
 }
