78fc19c6de6cd64dc1af891e36642e86d89f07ee
Redrield <redrield@gmail.com>
2018-10-06 17:21:51 -0400

    Transition to using OpenOptions for json::file (#12)
    * Changes json::file to create the file if it doesn't exist, and to open
    and append to it if it already exists
    * Add newline if appending to file
    
    Fixes #7

--- src/json.rs
+++ src/json.rs
@@ -8,9 +8,19 @@ use {Error, Target};
 
 /// Create a new JSON output that writes to a file
 pub fn file<T: AsRef<Path>>(name: T) -> Result<Target, Error> {
-    use std::fs::File;
+    use std::fs::{File, OpenOptions};
     use std::io::BufWriter;
-    let t = BufWriter::new(File::create(name)?);
+
+    let target = if name.as_ref().exists() {
+        let mut f = OpenOptions::new().write(true).append(true).open(name)?;
+        f.write_all(b"\n")?;
+
+        f
+    } else {
+        File::create(name)?
+    };
+
+    let t = BufWriter::new(target);
 
     Ok(Target::Json(Formatter {
         writer: Box::new(t),
