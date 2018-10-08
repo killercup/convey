6286d97f622da00df2ee61b1cb0b6ebcac3a66d1
Pascal Hertleif <killercup@gmail.com>
2018-10-08 12:28:21 +0200

    Derive tuple structs and add a test

--- output_derive/src/lib.rs
+++ output_derive/src/lib.rs
@@ -16,38 +16,55 @@ pub fn render_output(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
 
     let name = &ast.ident;
-    let fields: Vec<_> = match ast.data {
+    let render_span = match ast.data {
         Data::Struct(s) => match s.fields {
-            Fields::Named(..) => s
-                .fields
-                .iter()
-                .map(|x| {
-                    let x = x.ident.clone().unwrap();
-                    (x.to_string(), quote!(#x))
-                }).collect(),
-            Fields::Unnamed(..) => s
-                .fields
-                .iter()
-                .enumerate()
-                .map(|(i, _)| (i.to_string(), quote!(#i)))
-                .collect(),
+            Fields::Named(..) => {
+                let fields = s.fields.iter().map(|f| {
+                    let x = f.ident.clone().unwrap();
+                    quote!(#x)
+                });
+                let names = s
+                    .fields
+                    .iter()
+                    .map(|f| f.ident.clone().unwrap().to_string());
+                quote! {
+                    let mut span = output::components::span();
+                    #(
+                        span = span.add_item(#names);
+                        span = span.add_item(": ");
+                        span = span.add_item(output::components::text(&self.#fields.to_string()));
+                        span = span.add_item("\n");
+                    )*
+                    span.render_for_humans(fmt)?;
+                }
+            }
+            Fields::Unnamed(..) => {
+                let field_count = s.fields.iter().count();
+                let fields = (0..field_count)
+                        .fold(Vec::new(), |mut res, i| {
+                            res.push(quote! { span = span.add_item(output::components::text(&self.#i.to_string())); });
+                            if i < field_count - 1 {
+                                res.push(quote! { span = span.add_item(", "); });
+                            }
+                            res
+                        });
+
+                quote! {
+                    let mut span = output::components::span();
+                    span = span.add_item("(");
+                    #(#fields)*
+                    span = span.add_item(")");
+                    span.render_for_humans(fmt)?;
+                }
+            }
             _ => panic!("Unit structs not supported for now, sorry."),
         },
         _ => panic!("Only structs supported for now, sorry."),
     };
-    let names = fields.iter().map(|(name, _)| name);
-    let fields = fields.iter().map(|(_, ident)| ident);
     let exp = quote! {
         impl output::Render for #name {
             fn render_for_humans(&self, fmt: &mut output::human::Formatter) -> Result<(), output::Error> {
-                let mut span = output::components::span();
-                #(
-                    span = span.add_item(#names);
-                    span = span.add_item(": ");
-                    span = span.add_item(output::components::text(&self.#fields.to_string()));
-                    span = span.add_item("\n");
-                )*
-                span.render_for_humans(fmt)?;
+                #render_span
                 Ok(())
             }
 
