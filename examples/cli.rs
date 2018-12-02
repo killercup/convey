3bae06584f0ad21af91062d696912a96f1432f06
Pascal Hertleif <killercup@gmail.com>
2018-12-02 20:25:06 +0100

    Expand CLI example

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,16 +1,32 @@
 extern crate failure;
 #[macro_use]
+extern crate structopt;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
 extern crate convey;
 
+use structopt::StructOpt;
 use convey::{
     components::{newline, text},
     human, json,
 };
 
+/// Demo stuff
+#[derive(StructOpt)]
+struct Cli {
+    /// Output JSON instead of human readable messages
+    #[structopt(long = "json")]
+    json: bool,
+}
+
 fn main() -> Result<(), failure::Error> {
-    let mut out = convey::new()
-        .add_target(json::file("target/foo.log")?)
-        .add_target(human::stdout()?);
+    let args = Cli::from_args();
+    let mut out = if args.json {
+        convey::new().add_target(json::stdout()?)
+    } else {
+        convey::new().add_target(human::stdout()?)
+    };
 
     let x = 42;
 
@@ -24,6 +40,31 @@ fn main() -> Result<(), failure::Error> {
         newline(),
         span!(intense = true, ["intense text",]),
     ]))?;
+
+    #[derive(Serialize)]
+    struct ErrorMessage {
+        code: i32,
+        name: String,
+        message: String,
+    }
+
+    impl convey::Render for ErrorMessage {
+        render_for_humans!(self -> [
+            span!(fg = "white", bg = "black", [text(self.code.to_string()), text(" "),]),
+            span!(fg = "red", bg = "black", [text(&self.name),]),
+            newline(),
+            text("> "),
+            text(&self.message),
+        ]);
+
+        render_json!();
+    }
+
+    out.print(&ErrorMessage {
+        code: 42,
+        name: String::from("error"),
+        message: String::from("Oh god no"),
+    })?;
 
     Ok(())
 }
